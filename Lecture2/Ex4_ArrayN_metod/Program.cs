d46f230dd3bae4b221eb6fc57fa356b1c103dc2f
Anastasia S <aparicion@mail.ru>
2023-07-06 18:03:53 +0300

    L2 Task 4 Массив заполняется с помощью функции

---
+++ Lecture2/Ex4_ArrayN_metod/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти элеиент массива = find. В массиве array n-элементов. с помощью метода
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+      collection[index] = new Random().Next(1,10); 
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+{
+    Console.WriteLine(col[position]);
+    position++;
+}
+   
+}
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+
+
