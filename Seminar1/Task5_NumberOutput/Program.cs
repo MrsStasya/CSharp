0b22fdd5b068884aab725da7823ef9e55100a013
Anastasia S <aparicion@mail.ru>
2023-07-07 11:07:27 +0300

    Вывод числа от -n до n

--- Seminar1/Task5_NumberOutput/Program.cs
+++ Seminar1/Task5_NumberOutput/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Задача 5. Напишите программу, которая на входе принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до NConsole.WriteLine("Hello, World!");
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int minusnumber = 0 - number;
+
+if(number<minusnumber)
+{
+    for(int i=number; i<= minusnumber; i++)
+    {
+        Console.Write($"{i} ");
+    }
+}
+    else
+    {
+        for(int i=minusnumber; i<=number; i++)
+        {
+                    Console.Write($"{i} ");
+        }
+    }
