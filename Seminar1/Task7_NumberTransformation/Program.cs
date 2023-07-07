c101db36620b6e2d5f3c7e3deb2b944aef95e7cb
Anastasia S <aparicion@mail.ru>
2023-07-07 10:54:09 +0300

    Вывод последней цифры 3х значного числа изменения для красоты

--- Seminar1/Task7_NumberTransformation/Program.cs
+++ Seminar1/Task7_NumberTransformation/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 7. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
-Console.Write("Введите число");
+Console.Write("Введите число ");
 int number = int.Parse(Console.ReadLine());
-if(number>=100 && number <=999) Console.Write(number%10);
+if(number>=100 && number <=999) Console.Write($"Последняя цифра из введенного числа равна {number % 10}");
 else Console.Write ("Не верно");
